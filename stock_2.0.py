006d52d2008c9eba38a85e9352d72fb5c7267cd4
sheehanm4 <sheehan.m.w@gmail.com>
2021-10-25 22:25:14 -0400

    New stock script

--- stock_2.0.py
+++ stock_2.0.py
@@ -1,4 +1,4 @@
-from yfinance import Ticker
+import yfinance as yf
 
 
 #Define varaibles
@@ -8,7 +8,7 @@ ticker_summary = ('symbol','shortName','sector','industry','marketCap','ask',
 global_periods = ('1d','5d','1mo','3mo','6mo','1y','2y','5y','10y', 'ytd','max')
 global_intervals = ('1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo')
 
-class stock(Ticker):
+class stock():
 
 
     def __init__(self,ticker_symbol):
@@ -16,13 +16,11 @@ class stock(Ticker):
         stock(). 
         
         """
-
-        self._this_stock = Ticker(ticker_symbol)
-   
+        self._this_stock = yf.Ticker(ticker_symbol)
         self.period = '1y'
         self.interval = '1d'
-        self.ohlc_data = self.history(period = self.period,interval = self.interval)
-
+        self.ohlc_data = self._this_stock.history(period = self.period,interval = self.interval)
+        self.info = self._this_stock.info
 
     def get_bid_ask(self):
         '''
@@ -30,11 +28,9 @@ class stock(Ticker):
         Returns the bid, ask and spread of a stock object in list form
         Returns [bid,ask,spread]
         '''
-        
-        ask = self.info['ask']
-        bid = self.info['bid']
+        ask = self._this_stock.info['ask']
+        bid = self._this_stock.info['bid']
         spread = round(abs(ask - bid),3)
-
         return [bid,ask,spread]
 
     def summary(self):
@@ -45,3 +41,8 @@ class stock(Ticker):
         print('Printing Summary of the Ticker...')
         for k in ticker_summary:
             print('{} | {} '.format(k,self.info[k]))
+
+
+if __name__ == '__main__':
+    my_stock = stock('MSFT')
+    my_stock.summary()
